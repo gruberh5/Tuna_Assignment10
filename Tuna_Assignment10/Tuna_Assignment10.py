66c961a0ab51447cd8a7d6333a7f74ef67be42d3
Henry Gruber <gruberhw@mail.uc.edu>
2024-11-13 15:50:31 -0500

    Add project files.

---
+++ Tuna_Assignment10/Tuna_Assignment10.py
@@ -0,0 +1,28 @@
+import json
+import requests
+import pandas as pd
+
+# Name: Team TUNA (Henry Gruber, Lucas Ransick)
+# email:  {required}
+# Assignment Number: Assignment 10 
+# Due Date:   11/14/2024
+# Course #/Section:   IS 4010 - 001
+# Semester/Year:  Fall 2024
+# Brief Description of the assignment: API request, cleaned up, printed to console, converted to .csv located in working directory
+# Brief Description of what this module does. gets api request, prints, converts to .csv
+# Citations: our in class JSON VS project
+# Anything else that's relevant: n/a
+
+def TriviaAPI():
+    response = requests.get('https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple')
+    json_string = response.content
+    
+    parsed_json = json.loads(json_string)
+    for question in parsed_json['results']:
+        print(question['question'])
+        print(question['correct_answer'])
+    
+
+    
+TriviaAPI()
+
